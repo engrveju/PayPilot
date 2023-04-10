e57232335cade55e1cc36b206522cc8f8ab18679
engrveju <engrveju@gmail.com>
2023-04-10 12:08:05 +0100

    initial setup

---
+++ src/test/java/com/pay/paypilot/PayPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.paypilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
