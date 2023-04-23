ec7cb650337203c074cef40c562e17b1f36f5471
engrveju <engrveju@gmail.com>
2023-04-23 16:57:54 +0100

    Setting up swagger configuration and modified spring security

--- src/main/java/com/pay/paypilot/config/security/WebSecurity.java
+++ src/main/java/com/pay/paypilot/config/security/WebSecurity.java
@@ -2,16 +2,12 @@ package com.pay.paypilot.config.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -19,34 +15,35 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
+@Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(securedEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurity {
     private final JwtAuthFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-    private final PasswordEncoder passwordEncoder;
-    private final CustomUserDetailService userDetailService;
-    private String path = "/api/v1/auth";
-    private final String[] AUTH_WHITELIST = {
-            path +"/login", path +"/social-login", path + "/forgot-password", path + "/reset-password",
-            path + "/register", path + "/verify-code", path + "/references" ,
-            "/v3/api-docs/**",  "/configuration/**",   "/swagger*/**",
-            "/swagger-ui/**",  "/webjars/**", "/wallet/**", "/api/v1/wallet/verifyPayment/**",
-            path + "/register", path + "/verify-code", path + "/references",
-            path + "/forgot-password/**",
-            path + "/reset-password/**",
-            path + "/verify-token/**",
-            path + "/confirmRegistration",
-            path + "/**"
+    //  private final LogoutHandler logoutHandler;
+    private final String[] WHITE_LISTED_URLS = {  "/api/v1/customer/signup","/api/v1/customer/verifyRegistration/**"
+            ,"/api/v1/**","/api/v1/category/**", "/api/v1/subcategory/**", "/api/v1/finalizeTrans/**", "/api/v1/state/**", "/api/v1/products/new-arrivals",
+            "/api/v1/products/best-selling", "/api/v1/customer/resendVerificationToken/**",
+            "/authenticate",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/webjars/**",
+            "/v3/**",
+            "/account/creditAccount",
+            "/**"
     };
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
-                .csrf().disable()
+                .csrf()
+                .disable()
                 .authorizeHttpRequests()
-                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .requestMatchers(WHITE_LISTED_URLS)
+                .permitAll()
+//            .requestMatchers("/swagger-ui/**").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -54,7 +51,13 @@ public class WebSecurity {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .logout()
+                .logoutUrl("/api/v1/auth/logout")
+//            .addLogoutHandler(logoutHandler)
+                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+        ;
+
         return http.build();
     }
     @Bean
@@ -71,18 +74,18 @@ public class WebSecurity {
         };
     }
 
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        return authenticationProvider;
-    }
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration authenticationConfiguration    ) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+//    @Bean
+//    public AuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService());
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            AuthenticationConfiguration authenticationConfiguration    ) throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
---
+++ src/main/java/com/pay/paypilot/utils/JwtUtil.java
@@ -0,0 +1,23 @@
+package com.pay.paypilot.utils;
+
+import com.pay.paypilot.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JwtUtil {
+    private final UserRepository usersRepository;
+
+    public UserDetails loadByUsername(String email) throws UsernameNotFoundException {
+        return usersRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("Not Found"));
+    }
+
+    public String loggedUserMail(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
+    }
+}
